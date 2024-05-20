aed1c1cb52e0228d31f17e5ce94e1d70d2d65a35
Vinícius Führ de Borba <vinicius_fuhr-borba@outlook.com>
2024-05-20 19:49:14 -0300

    Initial commit

---
+++ src/config/index.ts
@@ -0,0 +1,13 @@
+export const PRODUCT_CATEGORIES = [
+  {
+    label: "UI Kist",
+    value: "ui_kits" as const,
+    featured: [
+      {
+        name: "Editor picks",
+        href: "#",
+        imageSrc: "/nav/ui-kits/mixed.jpg",
+      },
+    ],
+  },
+];
